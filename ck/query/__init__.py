22721d357fd8ddecaa36280289e8b13d7fbdfd5a
hcz <h.cz@qq.com>
2020-05-28 11:16:38 +0800

    Fix

--- ck/query/__init__.py
+++ ck/query/__init__.py
@@ -5,14 +5,16 @@ from ck.query import sql
 BaseAST = ast.BaseAST
 BaseExpression = ast.BaseExpression
 BaseStatement = ast.BaseStatement
-CallExpression = ast.CallExpression
-IdentifierExpression = ast.IdentifierExpression
-InitialStatement = ast.InitialStatement
-ListClauseStatement = ast.ListClauseStatement
-SimpleClauseStatement = ast.SimpleClauseStatement
-escape_text = ast.escape_text
+box = ast.box
+Call = ast.Call
 escape_buffer = ast.escape_buffer
+escape_text = ast.escape_text
 escape_value = ast.escape_value
-ValueExpression = ast.ValueExpression
+Identifier = ast.Identifier
+Initial = ast.Initial
+ListClause = ast.ListClause
+SimpleClause = ast.SimpleClause
+unbox = ast.unbox
+Value = ast.Value
 
 sql_template = sql.sql_template
