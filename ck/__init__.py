1b394d0b46464fbf78253b1a85c7ee7bef50bcd4
hcz <h.cz@qq.com>
2020-07-22 13:46:59 +0800

    Import query functions

--- ck/__init__.py
+++ ck/__init__.py
@@ -1,5 +1,9 @@
+from ck import query
 from ck import session
 
+
+sql_render = query.sql_render
+sql_template = query.sql_template
 
 LocalSession = session.LocalSession
 PassiveSession = session.PassiveSession
