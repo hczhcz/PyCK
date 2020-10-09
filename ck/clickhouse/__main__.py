cc6ca661e3ceb1e9ad243d41336d1c5da4889fea
hcz <h.cz@qq.com>
2020-10-09 14:35:14 +0800

    Fix process creation

--- ck/clickhouse/__main__.py
+++ ck/clickhouse/__main__.py
@@ -1,7 +1,7 @@
-import subprocess
+import posix
 import sys
 
 from ck.clickhouse import lookup
 
 
-subprocess.Popen([lookup.binary_file(), *sys.argv[1:]]).wait()
+posix.execv(lookup.binary_file(), sys.argv)
